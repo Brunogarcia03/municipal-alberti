932c4480fee4af1dab5738594342e35b5e4e7857
Brunogarcia03 <brunogalberti@gmail.com>
2026-01-19 09:12:18 -0300

    layout metadata preconnect 2v

--- src/app/layout.js
+++ src/app/layout.js
@@ -51,15 +51,8 @@ export default function RootLayout({ children }) {
           href="https://res.cloudinary.com/dshbxjhtw/image/upload/v1759754224/ICON_f25199ec4c.png"
           type="image/png"
         />
-        {process.env.NEXT_PUBLIC_BACKEND_URL && (
-          <>
-            <link rel="preconnect" href={process.env.NEXT_PUBLIC_BACKEND_URL} />
-            <link
-              rel="dns-prefetch"
-              href={process.env.NEXT_PUBLIC_BACKEND_URL}
-            />
-          </>
-        )}
+        <link rel="preconnect" href={process.env.NEXT_PUBLIC_BACKEND_URL} />
+        <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_BACKEND_URL} />
         <link rel="preconnect" href="https://res.cloudinary.com" />
         <link rel="dns-prefetch" href="https://res.cloudinary.com" />
       </head>
