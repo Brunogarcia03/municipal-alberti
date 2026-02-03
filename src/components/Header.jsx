fc14248ce9788fd4c6fcaa58adfbfcfbf7a18cbd
Brunogarcia03 <brunogalberti@gmail.com>
2026-02-03 11:33:07 -0300

    getRedes 6v

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ const NavList = [
 ];
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
   const [redes, setRedes] = useState(null);
   const [lastScrollY, setLastScrollY] = useState(0);
 
@@ -64,8 +64,6 @@ const Header = () => {
   }, []);
 
   useGSAP(() => {
-    if (isOpen === null) return;
-
     const tl = gsap.timeline({
       clearProps: "all",
     });
@@ -83,9 +81,9 @@ const Header = () => {
         menuLines?.forEach((el) => el.classList.remove("header--light"));
       })
         .to("#menu-wrapper", {
-          duration: 0.8,
-          height: "auto",
+          maxHeight: 1000,
           y: 0,
+          duration: 1,
           ease: "power2.out",
         })
         .fromTo(
@@ -126,11 +124,12 @@ const Header = () => {
         ease: "power2.in",
       })
         .to("#menu-wrapper", {
-          height: 0,
+          maxHeight: 0,
           y: -88,
-          duration: 0.4,
+          duration: 0.8,
           ease: "power2.inOut",
         })
+
         .add(() => {
           if (lenis) lenis.start();
           document.documentElement.style.overflow = "";
@@ -268,7 +267,7 @@ const Header = () => {
       </nav>
       <div
         id="menu-wrapper"
-        className="absolute top-0 left-0 w-full h-0 overflow-hidden text-white bg-black rounded-b-lg flex flex-col items-start pt-[3em] md:pt-[4em] px-[2em] pb-[1em] translate-y-[-88px]"
+        className="absolute top-0 left-0 max-h-0 w-full overflow-hidden text-white bg-black rounded-b-lg flex flex-col items-start pt-[3em] md:pt-[4em] px-[2em] pb-[1em] translate-y-[-88px]"
       >
         <div className="overscroll-contain flex flex-col md:flex-row items-center justify-between overflow-hidden w-full mt-[4em]">
           {NavList.map((item, index) => (
@@ -362,7 +361,7 @@ const Header = () => {
           </div>
           <div
             id="socials"
-            className="flex flex-col md:items-end justify-end w-full overflow-hidden"
+            className="flex flex-col md:items-end justify-end w-full will-change-transform overflow-hidden"
           >
             <p className="text-[1rem] leading-[1.4] font-normal">
               Redes Sociales
